e1e560e764aa649a6fff77b6cc256e4dd3d4b625
Amelia Davis <ameliaaltman22@gmail.com>
2020-01-26 22:41:31 -0700

    added 5 day forcast elements and resolved some issues with storage

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ var userSearch = $("#searchBox").val();
 // This is our API key
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
 var cityForecastEl = $("#city-forecast");
-var listGroup = $(".list-group")
-var cities = [];
+var listGroup = $(".list-group");
+var FiveDayEl = $("#5-Day");
+var day1 = $("#Blue");
+let cities = JSON.parse(localStorage.getItem("listGroup")) || []; // grab this array from local storage or set it to a blank array
+
+// set local storage
+function updateStorage () {
+  localStorage.setItem("listGroup", JSON.stringify(cities));
+}
+// on page load populate buttons list from local storage
+// $(document).ready(function(){
+//   $(".list-group").append(JSON.parse(localStorage.getItem("listGroup")));
+// }),
+
 
 //print cities list to the screen each  time one is added 
 function renderButtons(){
@@ -22,7 +34,10 @@ $("#search-button").on("click", function() {
      event.preventDefault(); // prevent default
      var userSearch = $("#searchBox").val(); 
      cities.push(userSearch);
+     cityForecastEl.empty();
+     day1.empty();
      renderButtons();
+     updateStorage();
 
 // Here we are building the URL we need to query the database
 // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
@@ -43,15 +58,16 @@ $.ajax({
     // console.log(currentTemp);
     var humidityEl = response.list[0].main.humidity;
     var windSpeedEl = response.list[0].wind.speed;
-    // var img = "http://openweathermap.org/img/w/" + userSearch+ data.weather[0].icon + ".png";
-    // console.log(img);
+    var img ="http://openweathermap.org/img/wn/10d@2x.png"
     // var uvIndexEl = response.main;// not sure where to grab this from
+    // variables for the 5 day forcast 
+    var day1ForcastDateEL = response.list[6].dt_txt;
+    console.log(day1ForcastDateEL);
+    var day1ForcastHumidityEl= response.list[6].main.humidity;
+    var day1forcastTempEl= response.list[6].main.temp;
 
-    var img = $("<img>").attr("src", queryURL + respons.list[0].weather[0].icon + ".png");
-    // var headline = result[i].headline.print_headline;
-    // var newHeandlineEL =$("<h1>");
-    // newHeandlineEL.text(headline);
-    // newArticle.append(newHeandlineEL);
+
+
 
     //Create div to hold the information we need from response
     var cityHeadline = $("<h2>");
@@ -60,6 +76,9 @@ $.ajax({
     var windSpeed = $("<p>");
     var imgDisplay = $("<img>");
     // var uvIndex = $("<p>");
+    var day1ForcastDate = $("<p>");
+    var day1ForcastHumidity = $("<p>");
+    var day1forcastTemp = $("<p>");
 
     // add the text information information to the appropriate HTML element
     cityHeadline.text(cityName + " " + Date)
@@ -67,9 +86,12 @@ $.ajax({
     humidity.text("Humidity: " + humidityEl + "%");
     windSpeed.text("Wind Speed: " + windSpeedEl);
     imgDisplay.attr("src", img);
+    // imgDisplay.attr("src", img + "@2x.png");
     // imgDisplay.attr("src","http://openweathermap.org/img/w/" + + img + ".png");
-    // console.log(temperature);
     // uvIndex.text = "UV Index: " + uvIndexEl;
+    day1ForcastDate.text("date" + day1ForcastDateEL);
+    day1ForcastHumidity.text("Humidity: " + day1ForcastHumidityEl + "%");
+    day1forcastTemp.text(day1forcastTempEl);
 
     //add to exisitng HTMl on page 
     cityForecastEl.append(cityHeadline);
@@ -77,11 +99,20 @@ $.ajax({
     cityForecastEl.append(humidity);
     cityForecastEl.append(windSpeed);
     // cityForecastEl.append(uvIndex);
-    cityForecastEl.append(imgDisplay);
+   cityForecastEl.append(imgDisplay);
     console.log(cityForecastEl);
+    day1.append(day1ForcastDate);
+    day1.append(imgDisplay);// add the image to the 5 day item
+    day1.append(day1ForcastHumidity);
+    day1.append(day1forcastTemp);
+    console.log(day1);
 
     //remove the Hide class from the entire div. 
     cityForecastEl.removeAttr("class", "hide");
+   
 
-});// closing the .then function
+  });// closing the .then function
 });// closing the search button listener function
+
+// function for the 5 day forcast 
+
